ad2a7c1faa797ffaf420896714d5f629ba366abd
Todd Anderson <bustardcelly@gmail.com>
2012-06-01 10:05:18 -0400

    rename of files and add of basic test on css concatenator.

---
+++ test/css_concatenate_test.py
@@ -0,0 +1,18 @@
+import sys, unittest
+sys.path.append('../')
+import css_concatenate
+
+class TestCSSConcat(unittest.TestCase):
+	"""Testing concatenation from css-concatenate script."""
+
+	def test_path_from_import_single_quotes(self):
+		url = css_concatenate.get_path_from_import('@import url(\'one.css\')')
+		self.assertTrue(url, 'one.css')
+
+	def test_path_from_import_double_quotes(self):
+		url = css_concatenate.get_path_from_import('@import url("one.css")')
+		self.assertTrue(url, 'one.css')
+
+suite = unittest.TestSuite()
+suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCSSConcat))
+unittest.TextTestRunner(verbosity=2).run(suite)
